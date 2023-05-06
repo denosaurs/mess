5f800b32c17356f3796c12461776a621450d6c1d
Elias Sjögreen <eliassjogreen1@gmail.com>
2023-05-06 12:29:49 +0200

    feat: break out (de)serializer (#2)

--- serializer_deserializer/types.ts
+++ serializer_deserializer/types.ts
@@ -1,4 +1,5 @@
-export interface SerializerDeserializer<T> {
+// deno-lint-ignore no-explicit-any
+export interface SerializerDeserializer<T = any> {
   contentType?: string;
   serialize(value: T): Uint8Array;
   deserialize(data: Uint8Array): T;
